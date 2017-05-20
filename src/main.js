14723c23862ca7b45b120a51df492dd0f3861de1
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2017-05-20 15:28:09 +0300

    directives

--- src/main.js
+++ src/main.js
@@ -1,13 +1,39 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+// import App from './App.vue'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+const app = new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+const app2 = new Vue({
+  el: '#app2',
+  data: {
+    message: `Вот когда вы загрузили эту страницу: ${new Date()}`
+  }
+})
+
+const app3 = new Vue({
+  el: '#app3',
+  data: {
+    seen: true
+  }
+})
+
+const app4 = new Vue({
+  el: '#app4',
+  data: {
+    todos: [
+      { text: 'Посадить дерево' },
+      { text: 'Построить дом' },
+      { text: 'Вырастить сына' }
+    ]
+  }
 })
